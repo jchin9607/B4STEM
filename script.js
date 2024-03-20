29c8930e9dbd45b77523297c700993e5202ff11c
jchin9607 <63731736+jchin9607@users.noreply.github.com>
2024-03-19 20:37:10 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-const gitlineElement = document.querySelector('.ghome');
-const about = document.getElementById("about");
+function setElementHeight(elementSelector, targetElementId) {
+  const element = document.querySelector(elementSelector);
+  const target = document.getElementById(targetElementId);
 
-const resizeObserver = new ResizeObserver(entries => {
-  for (let entry of entries) {
-    const aboutheight = entry.contentRect.height;
-    gitlineElement.style.height = aboutheight + "px";
-  }
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      const height = entry.contentRect.height;
+      element.style.height = height + "px";
+    }
+  });
+
+  resizeObserver.observe(target);
+}
+
+setElementHeight('.ghome', 'about');
+setElementHeight('.gres', 'resources');
+
+const gitlines = document.querySelectorAll('.gl');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (!entry.target.classList.contains("gitline-show") && entry.isIntersecting) {
+      entry.target.classList.add("gitline-show");
+    }
+  })
+}, {
+  threshold: 0.3,
 });
 
-resizeObserver.observe(about);
+gitlines.forEach(gitline => {
+  observer.observe(gitline);
+});
